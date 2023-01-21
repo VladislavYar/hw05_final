7a4c448df535bb529f8d7e094ae49b4db04421b7
Владислав Ярёменко <vlad.yaremenko.98@yandex.ru>
2023-01-21 04:33:59 +0300

    Debug.

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('posts.urls', namespace='posts')),
@@ -34,5 +33,4 @@ handler403 = 'core.views.permission_denied'
 
 if settings.DEBUG:
     import debug_toolbar
-
     urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
