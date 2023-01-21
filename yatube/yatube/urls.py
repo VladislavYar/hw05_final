76931a5c9fb4158b3bb2843c53817f650eed4eda
Владислав Ярёменко <vlad.yaremenko.98@yandex.ru>
2023-01-21 04:51:57 +0300

    Correct new dabug.

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -33,4 +35,9 @@ handler403 = 'core.views.permission_denied'
 
 if settings.DEBUG:
     import debug_toolbar
+
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
+
     urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
